752e9c7f46c2bd2f638934f0e643080a5ed84f8d
Harry Saitoti <harrysunkuli@gmail.com>
2023-07-12 10:56:17 +0000

    4inge

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""
-Check for subclass
-"""
+''' module: 4-inherits_from
+'''
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class"""
-    return(issubclass(type(obj), a_class))
+    '''the object is an instance of a class that inherited (directly or indirectly)
+    obj: an object
+    a_class: a class
+    returns None
+    '''
+    return type(obj) != a_class and isinstance(obj, a_class)
